98b288a6a2860d2724f6bc363983205fe01965eb
jeong-sik <yousleepwhen@gmail.com>
2021-04-15 15:49:18 +0900

    exercise 2 completed

--- exercises/chapter2/test/Main.purs
+++ exercises/chapter2/test/Main.purs
@@ -28,7 +28,7 @@ main = do
         Assert.equal 3.141592653589793 (circleArea 1.0)
       test "radius 3" do
         Assert.equal 28.274333882308138 (circleArea 3.0)
-        {-  Move this block comment starting point to enable more tests
+        
     suite "leftoverCents" do
       test "23" do
         Assert.equal 23 (leftoverCents 23)
@@ -36,5 +36,6 @@ main = do
         Assert.equal 56 (leftoverCents 456)
       test "-789" do
         Assert.equal (-89) (leftoverCents (-789))
+        {-  Move this block comment starting point to enable more tests
 
 -}
--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -2,9 +2,12 @@ module Test.MySolutions where
 
 import Prelude
 import Math (sqrt)
+import Data.Int (rem)
 
 pi = 3.141592653589793
 
 diagonal w h = sqrt (w * w + h * h)
 
 circleArea radius = pi * (radius * radius)
+
+leftoverCents n = rem n 100
